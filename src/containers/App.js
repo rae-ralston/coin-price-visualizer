904cb00faf35118808f0b854e4f4cf584c9945c4
Rachel Ralston <rachelralston@gmail.com>
2017-11-12 20:53:29 -0800

    generally wreaking havoc

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,38 +3,78 @@ import moment from 'moment'
 import './App.css';
 import { connect } from 'react-redux'
 import { UPDATE_PRICES } from '../constants';
-// import PriceBarGraph from './PriceBarGraph'
 // API for Current Price in USD: https://min-api.cryptocompare.com/data/price?fsym=LUN&tsyms=USD')
 
 class App extends Component {
 
-  componentDidMount() {
-    let closingPrices
+  fetchPrices = () => {
     fetch('https://min-api.cryptocompare.com/data/histohour?fsym=LUN&tsym=USD&limit=60&aggregate=3&e=CCCAGG')
       .then(rawResponse => rawResponse.json())
       .then(parsedResponse => {
-         closingPrices = parsedResponse.Data.map(priceObj => ({
+        return parsedResponse.Data.map(priceObj => {
+          return {
             time: moment(new Date( priceObj.time )).format("LTS"),
             price: priceObj.close
-          }))
+          }
+        })
+    })
+  }
+
+  componentDidMount() {
+    fetch('https://min-api.cryptocompare.com/data/histohour?fsym=LUN&tsym=USD&limit=60&aggregate=3&e=CCCAGG')
+      .then(rawResponse => rawResponse.json())
+      .then(parsedResponse => {
+        return parsedResponse.Data.map(priceObj => {
+          return {
+            time: moment(new Date( priceObj.time )).format("LTS"),
+            price: priceObj.close
+          }
+        })
+      })
+      .then(closingPrices => {
         this.props.dispatch({
           type: UPDATE_PRICES,
           prices: closingPrices
         })
-        console.log('=>',this.props)
       })
       .catch(e => console.error(e))
+    // const { store } = this.context
+    // this.unsubscribe = store.subscribe(() =>
+    //   this.forceUpdate()
+    // )
   }
 
-  render() {
-    console.log('***', this.context)
+  // componentWillUnmount() {
+  //   this.unsubscribe()
+  // }
 
+  render() {
+    const props = this.props
+    // const {store} = this.context
+    // const state = store.getState()
     return (
       <div className="App">
-        hai {this.props.data}
+        <button onGetPriceClick={this.fetchPrices}>Get Prices</button>
+        hai {props.data}
+        {props.prices}
       </div>
     );
   }
 }
 
-export default connect()(App);
+const mapStateToProps = state => {
+  return {
+    prices: state.prices
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    onGetPriceClick: (closingPrices) => dispatch({
+      type: UPDATE_PRICES,
+      prices: closingPrices
+    })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default App
--- src/reducers/prices.js
+++ src/reducers/prices.js
@@ -3,6 +3,7 @@ import { UPDATE_PRICES } from '../constants'
 const prices = (state = [], action) => {
   switch (action.type) {
     case UPDATE_PRICES:
+      console.log('in reducer', action)
       return [action.prices]
     default:
       return state
